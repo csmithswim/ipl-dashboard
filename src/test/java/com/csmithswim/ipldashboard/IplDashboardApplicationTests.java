95924bd16b316ae3c4d24a4234ff86e67c260156
Cullen S <cullensmi@gmail.com>
2022-02-04 14:23:11 -0500

    first commit of java brains walkthrough

---
+++ src/test/java/com/csmithswim/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csmithswim.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
